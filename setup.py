c96d4dfceef979df69bd90363039a7d26be76099
samuwall <samu.wall02@gmail.com>
2025-02-22 12:33:07 -0500

    fix tabs in `setup.py`

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup, find_packages
 
-#with open('requirements.txt') as f:
-#  requirements = f.read().splitlines()
-
 setup(
-  name='gpt-repl',
-  version='0.1.0',
-  packages=find_packages(),
-  include_package_data=True,
-  install_requires=[
+    name='gpt-repl',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
         "prompt-toolkit",
         "litellm",
         "rich",
@@ -16,9 +13,9 @@ setup(
         "ansiwrap_hotoffthehamster",
         "pygments",
     ],
-  entry_points={
-    'console_scripts': [
-      'gpt=gpt_repl.main:main',  # "gpt" command points to the main function in main.py
-    ],
-  },
+    entry_points={
+        'console_scripts': [
+        'gpt=gpt_repl.main:main',  # "gpt" command points to the main function in main.py
+        ],
+    },
 )
